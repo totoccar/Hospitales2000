a96e61b89cf4e085c7ef31b4e105d24fec4b0c67
franciscoruiz00 <franciscoruizg00@hotmail.com>
2024-09-26 11:11:47 -0300

    Add login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "./ui/LoginForm";
 
 export default function LoginPage() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-[#025951] via-[#04D99D] to-[#04D9B2] ">
+    <main className="flex flex-col items-center justify-center min-h-screen bg-zinc-50  ">
       <div className="mx-auto w-full max-w-[400px] flex flex-col space-y-2.5">
         <LoginForm />
       </div>
