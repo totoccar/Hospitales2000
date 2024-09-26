a595f2434ebd6dff14d6c4ef486ee34a607d283e
TobiasThiessen11 <tobiasthiessen@live.com.ar>
2024-09-26 15:41:58 -0300

    Fix Merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
 
 export default function Home() {
   return (
-<<<<<<< HEAD
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Prueba Branch Fran y Tobi
-=======
     <div className="grid items-center justify-items-center h-screen bg-fondo">
-        <h1 className="font-bold text-3xl text-acentos">HOLA RUKO</h1>
->>>>>>> c89335ba16b6c581ce37268873c3b2bd812b91c0
+        <h1 className="font-bold text-3xl text-acentos">Pagina Inicio</h1>
     </div>
   );
 }
