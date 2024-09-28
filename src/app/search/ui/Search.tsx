38361d73f2662af5d3f961bcc6532b6ba1276726
Julian Alconcher <alconcherjulian@gmail.com>
2024-09-28 13:06:22 -0300

    Fix logic

--- src/app/search/ui/Search.tsx
+++ src/app/search/ui/Search.tsx
@@ -88,7 +88,7 @@ export default function SearchForm() {
             </div>
           </div>
 
-          <Button type="submit" size="icon">
+          <Button type="submit" className='bg-primario' size="icon">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
             <span className="sr-only">Buscar</span>
           </Button>
