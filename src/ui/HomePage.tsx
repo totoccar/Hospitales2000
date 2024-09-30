452eda3dbc0bf481e70c6431e1f980c2906235ce
Antonio <totocarlos0@outlook.com>
2024-09-30 18:29:42 -0300

    Cambios, a ver si andan

--- src/ui/HomePage.tsx
+++ src/ui/HomePage.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
 import { UserRound, FileText, Search } from "lucide-react"
 
 export default function HomePage() {
