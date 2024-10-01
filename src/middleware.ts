5460d18d3a808d742340ada4bd82cd21cababcc4
Julian Alconcher <alconcherjulian@gmail.com>
2024-10-01 16:42:46 -0300

    Fix scheme

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,26 +9,20 @@ export async function middleware(req: NextRequest) {
 
   const publicRoutes = ['/login', '/api/auth'];
 
-  // Si la ruta es pública y el usuario ya está autenticado, redirigir al home
   if (token && pathname.startsWith('/login')) {
     return NextResponse.redirect(new URL('/', req.url));
   }
 
-  // Si la ruta es pública y el usuario no está autenticado, permitir acceso
   if (publicRoutes.some((route) => pathname.startsWith(route))) {
     return NextResponse.next();
   }
 
-  // Si no existe token, redirigir al login
   if (!token) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
-
-  // Continuar si el usuario está autenticado
   return NextResponse.next();
 }
 
-// Aplicar el middleware a todas las rutas
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
