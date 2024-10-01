40ba2976dba452c0ded64e21cf51ea7825c742c1
TobiasThiessen11 <tobiasthiessen@live.com.ar>
2024-10-01 17:27:01 -0300

    Fix NextAuth

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET});
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET});
 
   const { pathname } = req.nextUrl;
 
