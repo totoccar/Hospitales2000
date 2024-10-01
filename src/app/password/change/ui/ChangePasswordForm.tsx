b7d06a32a6fafba670c698efb763127c1ea1e7e5
Julian Alconcher <alconcherjulian@gmail.com>
2024-10-01 14:24:53 -0300

    Add US-3 (falta conectar id de la persona que esta con la sesion)

---
+++ src/app/password/change/ui/ChangePasswordForm.tsx
@@ -0,0 +1,169 @@
+'use client';
+import React, { useState } from 'react';
+import {
+    LockClosedIcon,
+    ExclamationCircleIcon,
+    KeyIcon,
+} from '@heroicons/react/24/outline';
+import { authenticatePassword, changePasswordAPI } from '@/src/lib/passwordActions';
+
+
+
+
+export default function ChangePasswordForm() {
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+    const [isPending, setIsPending] = useState(false);
+    const [successMessage, setSuccessMessage] = useState<string | null>(null);
+    const [isDisabled, setIsDisabled] = useState(false);  
+
+    const user_id = "cde3fea9-b222-49e6-8e53-24cc1ec6d0ba";
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+        setIsPending(true);
+
+        const passwordMatch = await authenticatePassword(
+            user_id,
+            formData.get('contrasena_actual') as string)
+
+        if (!passwordMatch) {
+            setErrorMessage('La contraseña actual es incorrecta');
+            setIsPending(false);
+        } else {
+
+            setErrorMessage(null);
+            setIsPending(false);
+            if (formData.get('nueva_contrasena') !== formData.get('confirm_contrasena')) {
+                setErrorMessage('Las contraseñas no coinciden');
+            } else {
+                changePasswordAPI(formData.get('nueva_contrasena') as string, user_id);
+                setSuccessMessage("La contraseña se ha cambiado correctamente.");
+                setIsDisabled(true);
+            }
+        }
+    };
+
+    return (
+        <div className="min-h-screen flex  items-center justify-center">
+            <form onSubmit={handleSubmit} className="space-y-3">
+                <div className="rounded-lg w-96 border border-gray-400 bg-gray-200 px-6 pb-4 pt-8">
+                    <h1 className="text-center mb-3 font-bold text-3xl text-[#025951]">
+                        Cambiar Contraseña
+                    </h1>
+                    {
+                        errorMessage && (
+                            <div className="flex items-center bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                                <ExclamationCircleIcon className="h-5 w-5 text-red-500 mr-2" />
+                                <span className="block sm:inline text-sm">{errorMessage}</span>
+                            </div>
+                        )
+                    }
+
+                    {
+                        successMessage && (
+                            <div className="flex items-center bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+                                <KeyIcon className="h-8 w-8 text-green-500 mr-4" />
+                                <span className="block sm:inline text-sm">{successMessage}</span>
+                            </div>
+                        )
+                    }
+                    <div className="w-full">
+                        <div>
+                            <label
+                                className="mb-3 mt-5 block text-m font-medium text-gray-900"
+                                htmlFor="dni"
+                            >
+                                Contraseña Actual
+                            </label>
+                            <div className="relative">
+                                <input
+                                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-m outline-2 placeholder:text-gray-500 disabled:bg-gray-100 disabled:text-gray-500 disabled:border-gray-200"
+                                    id="contrasena_actual"
+                                    type="password"
+                                    name="contrasena_actual"
+                                    placeholder="Ingrese su contraseña actual"
+                                    required
+                                    disabled={isDisabled}
+                                />
+                                <LockClosedIcon
+                                    className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                            </div>
+                        </div>
+                        <div className="mt-4">
+                            <label
+                                className="mb-3 mt-5 block font-medium text-m text-gray-900"
+                                htmlFor="password"
+                            >
+                                Nueva Contraseña
+                            </label>
+                            <div className="relative">
+                                <input
+                                    className="peer block w-full rounded-md border text-m border-gray-200 py-[9px] pl-10 outline-2 placeholder:text-gray-500 disabled:bg-gray-100 disabled:text-gray-500 disabled:border-gray-200"
+                                    id="confirm_contrasena"
+                                    type="password"
+                                    name="nueva_contrasena"
+                                    placeholder="Ingrese su nueva contraseña"
+                                    required
+                                    minLength={3}
+                                    disabled={isDisabled}
+                                />
+                                <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900  " />
+                            </div>
+                        </div>
+                        <div className="mt-4">
+                            <label
+                                className="mb-3 mt-5 block font-medium text-m text-gray-900"
+                                htmlFor="password"
+                            >
+                                Confirmar Contraseña
+                            </label>
+                            <div className="relative">
+                                <input
+                                    className="peer block w-full rounded-md border text-m border-gray-200 py-[9px] pl-10 outline-2 placeholder:text-gray-500 disabled:bg-gray-100 disabled:text-gray-500 disabled:border-gray-200"
+                                    id="contrasena"
+                                    type="password"
+                                    name="confirm_contrasena"
+                                    placeholder="Confirme su nueva contraseña"
+                                    required
+                                    minLength={3}
+                                    disabled={isDisabled}
+                                />
+                                <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="flex justify-center mt-6">
+                        <LoginButton isDisabled={isDisabled} isPending={isPending} />
+                    </div>
+                    <div className="text-center mt-4">
+                        <a href="/" className="text-sm text-gray-500 hover:underline">
+                            Volver al Inicio
+                        </a>
+                    </div>
+                    <div
+                        className="flex h-4 items-end space-x-1"
+                        aria-live="polite"
+                        aria-atomic="true"
+                    ></div>
+
+                </div>
+            </form>
+        </div>
+    );
+};
+
+function LoginButton({ isPending, isDisabled }: { isPending: boolean, isDisabled: boolean }) {
+    return (
+        <button
+            type="submit"
+            disabled={isPending || isDisabled}
+            className={`rounded-md p-2 bg-[#025951] hover:bg-[#04D99D] text-white font-bold mt-4 w-full ${isPending || isDisabled ? 'cursor-not-allowed opacity-50' : ''
+                }`}
+            aria-label="Cambiar Contraseña"
+        >
+            {isPending ? 'Cargando...' : isDisabled ? 'Contraseña Cambiada' : 'Cambiar Contraseña'}
+        </button>
+    );
+}
---
+++ src/lib/changePassword.ts
@@ -0,0 +1,148 @@
+// pages/api/changePassword.ts
+
+import bcrypt from 'bcryptjs';
+import prisma from './db';
+/** Primer funcion =') nunca olvidar */
+export async function getPassword(user_id: string): Promise<string> {
+    try {
+        const user = await prisma.usuario.findUnique({
+            where: { id: user_id },
+        });
+        console.log("Se obtuvo el usuario: ", user);
+
+
+        if (!user) {
+            throw new Error('User not found.');
+        }
+        console.log(user);
+
+
+        return user.contrasena;
+    } catch (error) {
+        console.error('Failed to fetch user:', error);
+        throw new Error('Failed to fetch user.');
+    }
+}
+
+
+export async function getPasswordByDocument(documento: string) {
+    try {
+        const user = await prisma.usuario.findUnique({
+            where: { numero_documento: documento }, // Cambiar aquí para buscar por documento
+        });
+
+
+        if (!user) {
+            throw new Error('User not found.');
+        }
+
+
+        console.log(user);
+        return user.contrasena; // Devuelve la contraseña del usuario
+    } catch (error) {
+        console.error('Failed to fetch user:', error);
+        throw new Error('Failed to fetch user.');
+    }
+}
+
+
+export async function changePassword(
+    nueva_contrasena: string,
+    user_id: string
+) {
+    try {
+        // Hash de la nueva contraseña antes de guardarla
+        const hashedPassword = await bcrypt.hash(nueva_contrasena, 10);
+
+
+
+
+        const user = await prisma.usuario.update({
+            where: { id: user_id },
+            data: { contrasena: hashedPassword },
+        });
+
+
+        if (!user) {
+            throw new Error('User not found.');
+        }
+
+
+        console.log('Password updated successfully for user:', user);
+    } catch (error) {
+        console.error('Failed to update password:', error);
+        throw new Error('Failed to update password.');
+    }
+}
+
+
+export async function getUserIdByDocument(documento: string) {
+    try {
+        const user = await prisma.usuario.findUnique({
+            where: {
+                numero_documento: documento,
+            },
+        });
+
+
+        if (!user) {
+            console.log('User not found');
+            return null;
+        }
+
+
+        console.log("El usuario de documento" + documento + "  es: " + user.nombre + " " + user.apellido); // Muestra el usuario en la consola
+        return user.id; // Devuelve el id del usuario
+    } catch (error) {
+        console.error('Error fetching user:', error);
+        return null;
+    }
+}
+
+
+
+
+
+
+export async function getUserEmailByDocument(user_document: string) {
+    try {
+        const user = await prisma.usuario.findUnique({
+            where: {
+                numero_documento: user_document,
+            },
+        });
+
+
+        if (!user) {
+            console.log('User not found');
+            return null;
+        }
+
+
+        console.log("El usuario de documento" + user_document + "  es: " + user.nombre + " " + user.apellido + " d correo :" + user.correo_electronico);
+        return user.correo_electronico;
+    } catch (error) {
+        console.error('Error fetching user:', error);
+        return null;
+    }
+}
+
+
+export async function getUserNameByDocument(user_document: string) {
+    try {
+        const user = await prisma.usuario.findUnique({
+            where: {
+                numero_documento: user_document,
+            },
+        });
+
+
+        if (!user) {
+            console.log('User not found');
+            return null;
+        }
+    } catch (error) {
+        console.error('Error fetching user:', error);
+        return null;
+    }
+}
