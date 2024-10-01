27a0ea4314f04ca1131f47ef4754235f95fc0d7a
TobiasThiessen11 <tobiasthiessen@live.com.ar>
2024-10-01 11:01:04 -0300

    Sign out provisional working

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -13,9 +13,9 @@ export async function authenticate(
     if (error instanceof AuthError) {
       switch (error.type) {
         case 'CredentialsSignin':
-          return 'Invalid credentials.';
+          return 'Credenciales incorrectas';
         default:
-          return 'Something went wrong.';
+          return 'Algo salio mal.';
       }
     }
     throw error;
