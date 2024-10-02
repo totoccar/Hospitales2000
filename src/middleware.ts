5dd21bbf0e465d570c267d08fe0bb259a9918963
Julian Alconcher <alconcherjulian@gmail.com>
2024-10-02 17:28:16 -0300

    ByPass login

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,24 +3,24 @@ import { NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET});
+  // const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET});
 
-  const { pathname } = req.nextUrl;
+  // const { pathname } = req.nextUrl;
 
-  const publicRoutes = ['/login', '/api/auth'];
+  // const publicRoutes = ['/login', '/api/auth'];
 
-  if (token && pathname.startsWith('/login')) {
-    return NextResponse.redirect(new URL('/', req.url));
-  }
+  // if (token && pathname.startsWith('/login')) {
+  //   return NextResponse.redirect(new URL('/', req.url));
+  // }
 
-  if (publicRoutes.some((route) => pathname.startsWith(route))) {
-    return NextResponse.next();
-  }
+  // if (publicRoutes.some((route) => pathname.startsWith(route))) {
+  //   return NextResponse.next();
+  // }
 
-  if (!token) {
-    return NextResponse.redirect(new URL('/login', req.url));
-  }
-  return NextResponse.next();
+  // if (!token) {
+  //   return NextResponse.redirect(new URL('/login', req.url));
+  // }
+  // return NextResponse.next();
 }
 
 export const config = {
