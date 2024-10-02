6fdb99e62d521275f00437e65395728b1e90387e
Julian Alconcher <alconcherjulian@gmail.com>
2024-10-02 17:30:56 -0300

    Fix responsive for search page

--- src/app/search/patient/ui/Search.tsx
+++ src/app/search/patient/ui/Search.tsx
@@ -95,9 +95,9 @@ export default function SearchForm() {
           </div>
         </div>
         </div>
-        <Button type="submit" className='bg-primario md:w-20 w-full' size="icon">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
-        <span className="sr-only">Buscar</span>
+        <Button type="submit" className='bg-primario md:w-20 w-48' size="icon">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
+          <span className="sr-only">Buscar</span>
         </Button>
       </div>
       </form>
