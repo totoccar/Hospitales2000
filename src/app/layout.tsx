f7615538b9714cbe555ccc30a0026ec8a43b0a7c
Antonio <totocarlos0@outlook.com>
2024-10-05 22:26:51 -0300

    Changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from 'next/font/google'
 import "./globals.css";
 import { cn } from "../lib/utils";
-import NavBarHandler from "../ui/NavBarHandler";
-import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({ subsets: ['latin'] })
 
