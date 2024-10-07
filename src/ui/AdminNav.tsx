4403183d73c2c91461328980e9070379201b87d3
Julian Alconcher <alconcherjulian@gmail.com>
2024-10-07 16:00:09 -0300

    Add dropdown menu to handle user session

--- src/ui/AdminNav.tsx
+++ src/ui/AdminNav.tsx
@@ -2,9 +2,10 @@
 
 import { Hospital, Search, PowerIcon } from 'lucide-react';
 import Link from 'next/link';
-import cerrarSesion from '../lib/session';
+import UserOptionsButton from './UserOptionsButton';
 
-export default function NavBarAdmin() {
+export async function NavBarAdmin() {
+  
   return (
     <nav className="bg-[#025951] text-[#F2F2F2] shadow-lg">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -17,8 +18,6 @@ export default function NavBarAdmin() {
             <div className="hidden sm:flex items-center">
               <span className="text-m mr-4 font-medium">Administrador</span>
             </div>
-
-            {/* Botón de Buscar Médico */}
             <Link
               href="/search/doctor"
               className="flex items-center px-2 py-2 rounded-md text-sm font-medium hover:bg-[#012623] hover:text-[#04D99D] transition-colors duration-200"
@@ -26,20 +25,7 @@ export default function NavBarAdmin() {
               <Search className="h-5 w-5 mr-1 sm:mr-2" />
               <span className="hidden sm:block">Buscar Medico</span>
             </Link>
-
-            {/* Botón de Cerrar Sesión */}
-            <form
-              onSubmit={async (e) => {
-                e.preventDefault();
-                await cerrarSesion();
-                window.location.reload();
-              }}
-            >
-              <button className="flex items-center justify-center gap-2 rounded-md p-2 sm:p-3 text-sm font-medium hover:bg-[#012623] transition-colors">
-                <PowerIcon className="w-5 sm:w-6" />
-                <span className="hidden sm:block">Cerrar Sesión</span>
-              </button>
-            </form>
+            <UserOptionsButton />
           </div>
         </div>
       </div>
