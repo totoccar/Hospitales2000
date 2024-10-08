4ddb04ec26ed71e868bc8661c24310b67fbb0fce
TobiasThiessen11 <tobiasthiessen@live.com.ar>
2024-10-08 18:49:34 -0300

    Appointment page working

--- src/app/appointment/view/page.tsx
+++ src/app/appointment/view/page.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { TipoDocumentoEnum } from '@prisma/client';
+import MaxWidthWrapper from '@/src/ui/MaxWidthWrapper';
+import { getAmountDoctorsByQuery } from '@/src/lib/searchdoctor';
+import Pagination from '../../search/ui/Pagination';
+import { getDni } from '../../lib/actions';
+import { getCitasById, getIdByDni } from '@/src/lib/searchappointments';
+import Table from './ui/Table';
 
-export default function AppointmentViewPage() {
+export default async function AppointmentView({searchParams,
+}: {
+  searchParams?: {
+    page?: string;
+}
+}){
+  const dni= await getDni();
+  const id= await getIdByDni(dni);
+  const currentPage = Number(searchParams?.page) || 1;
   return (
-    <div>Appointment View Page</div>
+    <MaxWidthWrapper>
+    <div className="bg-fondo rounded-lg shadow-md p-6 mt-5">
+        <h2 className="text-xl font-bold mb-4">Citas Asignadas</h2>
+        <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        </div>
+        <Table pacienteId={id} currentPage={currentPage}/>
+        </div>
+        <div className="mt-5 flex w-full justify-center">
+      </div>
+    </MaxWidthWrapper>
   )
 }
---
+++ src/app/appointment/view/ui/Table.tsx
@@ -0,0 +1,88 @@
+import { getCitasById } from '@/src/lib/searchappointments';
+import { DeleteAppointment, ViewDoctor } from '@/src/ui/Buttons';
+
+export default async function Table({
+  pacienteId,
+  currentPage,
+}: {
+  pacienteId: string | null;
+  currentPage: number;
+}) {
+  const citas = await getCitasById(pacienteId ?? "");
+  return (
+    <div className="mt-6 flow-root">
+      <div className="inline-block min-w-full align-middle">
+        <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
+          {/* Para pantallas pequeñas */}
+          <div className="md:hidden">
+            {citas?.map((cita, index) => (
+              <div key={index} className="mb-2 w-full rounded-md bg-fondo p-4 border-b pb-4">
+                <div className="flex justify-between bg-fondo p-2 items-left">
+                  <div>
+                    <p>{new Date(cita.fecha_hora).toLocaleDateString()}</p>
+                    <p className="text-xl text-gray-500">{new Date(cita.fecha_hora).toLocaleTimeString()}</p>
+                  </div>
+                  <div className="flex flex-col items-right">
+                    <p className="text-gray-500">
+                      {cita.nombre} {cita.apellido} ({cita.especialidad})
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {/* Tabla para pantallas grandes */}
+          <table className="hidden min-w-full text-gray-900 md:table">
+            <thead className="rounded-lg text-left text-lg font-semibold text-gray-700">
+              <tr>
+                <th scope="col" className="px-4 py-3 font-medium sm:pl-4 text-center">Día</th>
+                <th scope="col" className="px-3 py-3 font-medium text-center">Horario</th>
+                <th scope="col" className="px-3 py-3 font-medium text-center">Médico</th>
+                <th scope="col" className="px-3 py-3 font-medium text-center">Acciones</th>
+              </tr>
+            </thead>
+            <tbody className="bg-white text-sm">
+              {citas?.length === 0 ? (
+                <tr>
+                  <td colSpan={4} className="text-center py-3">
+                    No hay citas para mostrar
+                  </td>
+                </tr>
+              ) : (
+                citas?.map((cita, index) => (
+                  <tr
+                    key={index}
+                    className="w-full border-b border-gray-300 last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
+                  >
+                    {/* Día */}
+                    <td className="whitespace-nowrap py-3 pl-4 pr-3 text-center">
+                      <div className="flex items-center justify-center">
+                        <p>{new Date(cita.fecha_hora).toLocaleDateString()}</p>
+                      </div>
+                    </td>
+
+                    {/* Horario */}
+                    <td className="whitespace-nowrap px-3 py-3 text-center">
+                      {new Date(cita.fecha_hora).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+                    </td>
+
+                    {/* Médico */}
+                    <td className="whitespace-nowrap px-3 py-3 text-center">
+                      {cita.nombre} {cita.apellido} <span className="text-gray-500">({cita.especialidad})</span>
+                    </td>
+
+                    {/* Acciones */}
+                    <td className="py-3 px-3 text-center">
+                        <DeleteAppointment />
+                    </td>
+                  </tr>
+                ))
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
