575737e2ec05baeb6027d6c2560a696f1ee1d814
Antonio <totocarlos0@outlook.com>
2024-10-10 19:46:08 -0300

    Cambios UI

--- src/app/appointment/calendar/[id]/page.tsx
+++ src/app/appointment/calendar/[id]/page.tsx
@@ -34,49 +34,48 @@ export default function SecCalendar({ params }: { params: { id: string } }) {
 
 
   return (
-    <MaxWidthWrapper className="border-gray-500">
-      <h2 className="text-xl font-bold">Selecciona el día y el horario que desees</h2>
-      <h3 className="text-md">Puedes agregar una aclaración para tu médico</h3>
+    <MaxWidthWrapper>
+      <div className="mt-10 bg-white text-center w-full rounded-xl shadow  border m-3 p-6">
+        <h2 className="text-xl font-bold mb-2">Seleccione el dia que quiera visualizar</h2>
+        <h3 className="text-md mb-4">Podra ver los turnos disponibles para el dia seleccionado</h3>
 
-      <div className="md:flex flex-row gap-4">
-        <Calendar
-          mode="single"
-          selected={date}
-          onSelect={(selectedDate) => {
-            setDate(selectedDate);
-            if (selectedDate) fetchDoctorTurns(selectedDate);
-          }}
-          className="rounded-xl shadow border m-3 bg-white"
-          disabled={(date) => isWeekend(date) || date < new Date()}
-          required
-        />
+        <div className="md:flex flex-row gap-4">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={(selectedDate) => {
+              setDate(selectedDate);
+              if (selectedDate) fetchDoctorTurns(selectedDate);
+            }}
+            className=" rounded-xl shadow border m-3 bg-white"
+            disabled={(date) => isWeekend(date) || date < new Date()}
+            required
+          />
 
-        <div className="bg-white text-center w-full rounded-xl shadow m-3 p-3 text-gray-500">
-          <h1 className="text-xl">Resumen de tu cita</h1>
-          {turnos.length > 0 ? (
-            <div>
-              {turnos.map((turno, index) => (
-                <div key={index}>{`Turno ${index + 1}: ${JSON.stringify(turno)}`}</div>
-              ))}
-            </div>
-          ) : (
-            <p>No hay turnos disponibles.</p>
-          )}
+          <div className="bg-white  text-center w-full rounded-xl shadow m-3 p-3 text-gray-500">
+            <h1 className="text-xl">Resumen de tu cita</h1>
+            {turnos.length > 0 ? (
+              <div>
+                {turnos.map((turno, index) => (
+                  <div key={index}>{`Turno ${index + 1}: ${JSON.stringify(turno)}`}</div>
+                ))}
+              </div>
+            ) : (
+              <p>No hay turnos disponibles.</p>
+            )}
+          </div>
+        </div>
+
+        <div className="w-full md:w-auto lg:w-auto bg-white m-3 p-3 rounded-xl shadow">
+          <Label className="mb-2 block text-center">Descripción</Label>
+          <textarea
+            id="description"
+            className="w-full p-2 border rounded-md resize-none"
+            rows={4}
+            placeholder="Añadir una descripción o pequeña aclaración"
+          />
         </div>
       </div>
-
-      <div className="w-full md:w-auto lg:w-auto bg-white m-3 p-3 rounded-xl shadow">
-        <Label className="mb-2 block text-center">Descripción</Label>
-        <textarea
-          id="description"
-          className="w-full p-2 border rounded-md resize-none"
-          rows={4}
-          placeholder="Añadir una descripción o pequeña aclaración"
-        />
-      </div>
-      <Button type="submit" className="w-full bg-primario">
-        Solicitar cita
-      </Button>
     </MaxWidthWrapper>
   );
 }
