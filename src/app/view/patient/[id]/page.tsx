f41640291cfe3ec0ee40309c1557b396982137d5
BrendaMartinez103 <brenh_97@hotmail.com>
2024-10-15 15:53:26 -0300

    editar paciente listo

--- src/app/view/patient/[id]/page.tsx
+++ src/app/view/patient/[id]/page.tsx
@@ -1,17 +1,43 @@
 
-import { Button } from "@/src/components/ui/button";
-import { getObraSocialById, getUbicacionById, getUsuarioConRolesById,getUsuarioById } from "@/src/lib/getUsuarioById";
+import { getObraSocialById, getUbicacionById, getUsuarioById } from "@/src/lib/getUsuarioById";
+//import ClientButtons from "@/src/components/temp/deletePatientButton";
+import ClientButtons from "@/src/components/temp/modifyPatientButton";
+import { getRole } from "@/src/app/lib/actions";
 import Link from "next/link";
 
 export default async function Component({ params }: { params: { id: string } }) {
-  const id = params.id as string;
-  const usuarioSecretaria = await getUsuarioConRolesById(id); 
-  const usuario = await getUsuarioById(id);
 
-  let ubicacionUsuario = null;
-  
-  if (usuario?.paciente) {
-    ubicacionUsuario = await getUbicacionById(usuario.paciente.ubicacion_id);
+  let disabled;
+  let disabledEdit;
+  //Get user role.
+  const role = await getRole();
+  const mapRoles = {
+    'Paciente': 'Paciente',
+    'Medico': 'Medico',
+    'Secretaria': 'Secretaria',
+    'Administrador': 'Administrador',
+  }
+  if (role != mapRoles.Administrador) {
+    disabled = true;
+  } else {
+    disabled = false;
+  }
+  if (role != mapRoles.Secretaria) {
+    disabledEdit = true;
+  } else {
+    disabledEdit = false;
+  }
+  const formatDate = (date: Date): string => {
+    const day = String(date.getDate()).padStart(2, '0'); // Asegura dos dígitos para el día
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Meses en JavaScript son de 0 a 11, así que sumamos 1
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
+  const id = params.id as string;
+  const usuario = await getUsuarioById(id);
+  let ubicacionUsuario=null;
+  if (usuario.paciente){
+  ubicacionUsuario = await getUbicacionById(usuario.paciente.ubicacion_id);
   }
 
   const DisplayField = ({ label, value }: { label: string; value: string }) => (
@@ -31,29 +57,23 @@ export default async function Component({ params }: { params: { id: string } }) 
         <DisplayField label="Número de documento" value={usuario.numero_documento} />
         <DisplayField label="Nombre" value={usuario.nombre} />
         <DisplayField label="Apellido" value={usuario.apellido} />
-        <DisplayField label="Fecha de nacimiento" value={usuario.paciente?.fecha_nacimiento.toDateString() || ""} />
-        <DisplayField label="Número de teléfono" value={usuario.paciente?.numero_telefono || ""} />
-        <DisplayField label="Lugar de nacimiento" value={usuario.paciente?.lugar_nacimiento || ""} />
-        <DisplayField label="Contacto de emergencia" value={usuario.paciente?.contacto_emergencia || ""} />
-        <DisplayField label="Calle" value={ubicacionUsuario?.calle || ""} />
-        <DisplayField label="Número" value={ubicacionUsuario?.numero || ""} />
-        <DisplayField label="Código postal" value={ubicacionUsuario?.codigo_postal || ""} />
-        <DisplayField label="Ciudad" value={ubicacionUsuario?.ciudad || ""} />
-        <DisplayField label="Provincia" value={ubicacionUsuario?.provincia || ""} />
+        <DisplayField label="Fecha de nacimiento" value={usuario.paciente?.fecha_nacimiento ? formatDate(usuario.paciente.fecha_nacimiento) : ""} />
+        <DisplayField label="Número de teléfono" value={usuario.paciente?.numero_telefono||""} />
+        <DisplayField label="Lugar de nacimiento" value={usuario.paciente?.lugar_nacimiento||""} />
+        <DisplayField label="Contacto de emergencia" value={usuario.paciente?.contacto_emergencia||""} />
+        <DisplayField label="Calle" value={ubicacionUsuario?.calle||""} />
+        <DisplayField label="Número" value={ubicacionUsuario?.numero||""} />
+        <DisplayField label="Código postal" value={ubicacionUsuario?.codigo_postal||""} />
+        <DisplayField label="Ciudad" value={ubicacionUsuario?.ciudad||""} />
+        <DisplayField label="Provincia" value={ubicacionUsuario?.provincia||""} />
         <DisplayField label="Correo electrónico" value={usuario.correo_electronico} />
-        <DisplayField label="Obra social" value={usuario.paciente?.obra_social_id ? await getObraSocialById(usuario.paciente.obra_social_id) || "" : "No tiene obra social"} />
+        <DisplayField label="Obra social" value={ usuario.paciente?.obra_social_id ? await getObraSocialById(usuario.paciente.obra_social_id) || "": "No tiene obra social"}/>
       </div>
       <div className="flex justify-end space-x-4 mt-6">
-        {usuarioSecretaria && (  // Verifica si el usuario tiene el rol de secretaria
-          <Link href={`/view/patient/${id}/editPatient`}>
-            <Button variant="outline">Editar</Button>
-          </Link>
-        )}
-        <Button disabled={true} variant="destructive">Eliminar</Button>
-      </div>
-      <p className="text-sm text-gray-500 mt-4">Solo disponible para rol de secretaria</p>
+        <Link href={`/view/patient/${id}/editPatient`}>
+        <ClientButtons id={id} disabledEdit={disabledEdit}/>
+        </Link>
+      </div> 
     </div>
   );
 }
-
-
