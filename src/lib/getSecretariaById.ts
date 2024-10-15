5aae76d527889b9d6f06f26070a6107fcbe4e1a7
BrendaMartinez103 <brenh_97@hotmail.com>
2024-10-15 16:57:43 -0300

    cambios en secretaria

--- src/lib/getSecretariaById.ts
+++ src/lib/getSecretariaById.ts
@@ -1,16 +1,6 @@
 import prisma from "./db";
 
-export async function getUsuarioConRolesById(id: string) {
-  return prisma.usuario.findUnique({
-    where: { id },
-    include: {
-      secretaria: false,  
-      medico: false,      
-      paciente: false,    
-      admin: true,       
-    },
-  });
-}
+
 export async function getUsuarioById(usuarioId: string) {
   try {
     const usuario = await prisma.usuario.findUnique({
