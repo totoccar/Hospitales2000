b85e7273735cd0418e100e1ccd2d9ebe55b10480
Antonio <totocarlos0@outlook.com>
2024-11-07 19:14:59 -0300

    Enviar Email al modificar cita

--- src/app/api/modify-appointment/route.ts
+++ src/app/api/modify-appointment/route.ts
@@ -1,26 +1,27 @@
+// app/api/modify-appointment/route.ts
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
-import { ModifyEmail } from "../ui/ModifyEmail";
+import { ModifyEmail } from "../../appointment/modify/ui/ModifyEmail";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
-  const {email, doctor,especialidad,fecha } = body;
+  const { email, doctor, fecha } = body;
 
-  if (!doctor || !email || !especialidad || !fecha) {
+  if (!doctor || !email || !fecha) {
     return NextResponse.json(
       { error: "Missing required fields" },
       { status: 400 }
     );
   }
-  console.log("email", email);
+
   const resend = new Resend(process.env.H2000_API);
 
   try {
     const { data, error } = await resend.emails.send({
       from: `Hospitales 2000 <h2000_informacion@hospitales2000.tech>`,
       to: email,
-      subject: "Tu cita ha sido modificada - Mira los detalles", 
-      react: ModifyEmail({ email, doctor,especialidad,fecha}), 
+      subject: "Tu cita ha sido modificada - Mira los detalles",
+      react: ModifyEmail({ email, doctor, fecha }),
     });
 
     if (error) {
